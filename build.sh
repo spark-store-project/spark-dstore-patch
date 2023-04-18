1ca55ed81c138d5f94ae7c7374fa0e88e023526c
shenmo <jifengshenmo@outlook.com>
2023-04-18 23:22:30 +0800

    提供deepin-app-store-tool

--- build.sh
+++ build.sh
@@ -1,54 +1,7 @@
 #!/bin/bash
 
-echo "build.sh修改自柚柚的 https://gitee.com/deepin-opensource/one-cuter"
 
-echo "检查依赖"
-
-DEPEND=`dpkg -l | grep   qt5-default`
-if [ "$DEPEND" = "" ] ; then 
-echo "未安装依赖：qt5-default 本脚本退出"
-exit 0
-fi
-
-DEPEND=`dpkg -l | grep   g++`
-if [ "$DEPEND" = "" ] ; then 
-echo "未安装依赖：g++ 本脚本退出"
-exit 0
-fi
-
-DEPEND=`dpkg -l | grep   fakeroot`
-if [ "$DEPEND" = "" ] ; then 
-echo "未安装依赖：fakeroot 本脚本退出"
-exit 0
-fi
-
-
-echo "依赖检查通过，开始编译"
-VERSION="1.2.2"
-ARCH=`dpkg --print-architecture`
-cd `dirname $0`
-
-# 编译
-echo "-------------------"
-echo "开始编译"
-mkdir -p build
-qmake src/spark-dstore-patch.pro -spec linux-g++ CONFIG+=qtquickcompiler -o build/
-cd build/
-make -j
-cd ..
-echo "编译完成"
-echo "-------------------"
-#echo "更新翻译"
-#lrelease ./translations/*.ts
-
-
-
-#放置编译好的文件
-
-mv build/spark-dstore-patch pkg/opt/durapps/spark-dstore-patch/spark-dstore-patch
-#cp translations/*.qm dabao/extract/opt/apps/top.yzzi.onecuter/files/translations/
-rm -rf build
-
+VERSION=1.3
 #打包
 echo "构建软件包"
 SIZE=`du -s ./pkg/opt`
@@ -56,18 +9,17 @@ SIZE=`echo ${SIZE%%.*}`
 # 生成control文件
 echo 生成control文件
 echo "版本号为$VERSION，可以在脚本中修改"
-echo "检测到编译机的架构为$ARCH"
 echo "检测到安装后的目录大小为$SIZE"
 cat  << EOF >pkg/DEBIAN/control
 Package: spark-dstore-patch
 Version: $VERSION
-Architecture: $ARCH
+Architecture: all
 Maintainer: shenmo <shenmo@spark-app.store>
 Installed-Size: $SIZE
-Depends: libc6 (>= 2.25)
+Depends: bash
 Section: admin
 Priority: optional
-Homepage: https://gitee.com/deepin-community-store/spark-dstore-patch
+Homepage: https://gitee.com/spark-community-works-collections/spark-dstore-patch
 Description: Patch for deepin-app-store packages
 
 EOF
